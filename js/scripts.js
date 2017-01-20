94ac365fd21265b94d9481d1a1ef037cbccccbe0
Ethan Luts and Jayeson Kiyabu <jayeson@>
2017-01-19 16:13:57 -0800

    add check for perfect squarestring

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 
-function cryptonize(input){
+function removePunc(input){
   var crypto = input.replace(/\W/g,"");
   return crypto;
 }
 
 function length(input){
-  var stringLength = (input.replace(/\W/g,"")).length;
+  var stringLength = input.replace(/\W/g,"").length;
   return stringLength;
+}
+
+function isPerfect(input) {
+  if(Number.isInteger(Math.sqrt(input.replace(/\W/g,"").length))) {
+    return true;
+  }else{
+    return false;
+  }
 }
 
 
@@ -15,8 +23,8 @@ $(document).ready(function() {
   $("form#cryptosquare").submit(function() {
     event.preventDefault();
     var input = $("#inputString").val();
-    console.log(result);
-    $("#result").text(cryptonize(input));
     console.log(length(input));
+    $("#result").text(removePunc(input));
+    console.log(isPerfect(input));
   });
 });
